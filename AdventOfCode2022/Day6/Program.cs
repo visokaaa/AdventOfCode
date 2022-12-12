25c78e3b7fa855f1f6e75dbfa2564d5be0b3b5b6
kujtimvisoka <kujtim@frakton.com>
2022-12-12 14:12:13 +0100

    Day 6

---
+++ AdventOfCode2022/Day6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Input.txt");
+var text = File.ReadAllText(path);
+
+//Part one
+Console.WriteLine(FindStartOfPacketMarker(text, 4));
+
+//Part two
+Console.WriteLine(FindStartOfPacketMarker(text, 14));
+
+
+
+int FindStartOfPacketMarker(string text, int characterSize)
+{
+    var characters = text.ToCharArray();
+    for (int i = 0; i < characters.Length - characterSize; i++)
+    {
+        var nextFourChars = text.Substring(i, characterSize);
+
+        if (nextFourChars.Distinct().Count() < characterSize)
+        {
+            continue;
+        }
+
+        return  i + characterSize;
+    }
+
+    return -1;
+}
+
