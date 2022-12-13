f0aebba9451abfefb75cd1778c7e1af634a9ba40
kujtimvisoka <kujtim@frakton.com>
2022-12-13 11:42:26 +0100

    Day 7

---
+++ AdventOfCode2022/Day7/Models/File.cs
@@ -0,0 +1,39 @@
+﻿namespace Day7.Models;
+
+internal class File
+{
+    public string Name { get; set; }
+    public bool IsDirectory { get; set; }
+    public File ParentDirectory { get; set; }
+    public List<File> Children { get; set; } = new List<File>();
+    private long size = 0;
+    public long Size
+    {
+        get { return IsDirectory ? Children.Sum(_ => _.Size) : size; }
+        set { size = value; }
+    }
+
+    public void AddFile(File file)
+    {
+        if (!Children.Contains(file))
+        {
+            Children.Add(file);
+        }
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        var file = obj as File;
+
+        return Name.Equals(file.Name);
+    }
+
+    public override int GetHashCode()
+    {
+        return Name.GetHashCode();
+    }
+}
---
+++ AdventOfCode2022/Day7/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using File = Day7.Models.File;
+
+var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Input.txt");
+var lines = System.IO.File.ReadAllLines(path);
+
+var move = "$ cd";
+var dir = "dir";
+var ls = "$ ls";
+
+var root = new File
+{
+    Name = "root",
+    IsDirectory = true,
+    ParentDirectory = null
+};
+
+var currentDirectory = root;
+
+for (int i = 1; i < lines.Length; i++)
+{
+    var line = lines[i];
+    if(line.Equals(ls))
+    {
+        continue;
+    }
+
+    if(line.StartsWith(dir))
+    {
+        var name = line.Substring(line.IndexOf(" ") + 1);
+        currentDirectory.AddFile(new File { Name = name, IsDirectory = true, ParentDirectory = currentDirectory });
+    } 
+    else if(line.Equals("$ cd .."))
+    {
+        currentDirectory = currentDirectory.ParentDirectory;
+    }
+    else if(line.StartsWith("$ cd"))
+    {
+        var name = line.Substring(line.LastIndexOf(" ") + 1);
+        currentDirectory = currentDirectory.Children.First(x => x.Name == name);
+    } 
+    else
+    {
+        var sizeAndName = line.Split(" ");
+        var size = int.Parse(sizeAndName[0]);
+        var name = sizeAndName[1];
+        currentDirectory.AddFile(new File { Name = name, IsDirectory = false, ParentDirectory = currentDirectory, Size = size });
+    }
+}
+
+var filteredDirectories = new List<File>();
+var totalDiskSize = 70000000l;
+var sizeToRemove = 30000000 - (totalDiskSize - root.Size);
+var directorySizeToDelete = 30000000l;
+
+FilterDirectories(root);
+
+void FilterDirectories(File file)
+{
+    if(file.IsDirectory)
+    {
+        if(file.Size <= 100000)
+        {
+            filteredDirectories.Add(file);
+        }
+        else if(file.Size >= sizeToRemove && file.Size < directorySizeToDelete) {
+            directorySizeToDelete = file.Size;
+        }
+        file.Children.ForEach(child => FilterDirectories(child));
+    }
+}
+
+//Part one
+Console.WriteLine(filteredDirectories.Sum(_ => _.Size));
+
+//Part two
+Console.WriteLine(directorySizeToDelete);
+
+
+
